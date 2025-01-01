045e4e1b9fcb02e04e00d75473ad252ead7a5a55
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-01 14:13:38 +0530

    home

--- src/app/view/Navbar.tsx
+++ src/app/view/Navbar.tsx
@@ -9,15 +9,15 @@ const Navbar: React.FC = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">Aryan Bhimani</span>
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
           <Link href="/About" className="mr-5 hover:text-gray-900">About</Link>
-          <Link href="/blog" className="mr-5 hover:text-gray-900">Blog</Link>
-          <Link href="/projects" className="mr-5 hover:text-gray-900">Projects</Link>
+          <Link href="/Blog" className="mr-5 hover:text-gray-900">Blog</Link>
+          <Link href="/Projects" className="mr-5 hover:text-gray-900">Projects</Link>
         </nav>
-        <Link href="/contact">
+        <Link href="/Contact">
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Contact
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
