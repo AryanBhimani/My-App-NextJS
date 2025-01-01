3bb4d39151da1d596194c5b67f1a44d47363fa44
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-01 13:59:10 +0530

    home

---
+++ src/Page/Blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Blog Page</h1>
+            <p>This is where the blog posts will be displayed.</p>
+        </div>
+    );
+};
+
+export default Blog;
