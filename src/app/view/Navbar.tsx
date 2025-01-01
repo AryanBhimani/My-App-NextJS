d2ece70c7f6a64a31da3514b02e46fdc590367ad
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-01 13:53:23 +0530

    home

--- src/app/view/Navbar.tsx
+++ src/app/view/Navbar.tsx
@@ -14,11 +14,12 @@ const Navbar: React.FC = () => {
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
           <Link href="/About" className="mr-5 hover:text-gray-900">About</Link>
-          <Link href="/Blog" className="mr-5 hover:text-gray-900">Blog</Link>
-          <Link href="/projects" className="mr-5 hover:text-gray-900">projects</Link>
+          <Link href="/blog" className="mr-5 hover:text-gray-900">Blog</Link>
+          <Link href="/projects" className="mr-5 hover:text-gray-900">Projects</Link>
         </nav>
         <Link href="/contact">
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Contact
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Contact Us
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
