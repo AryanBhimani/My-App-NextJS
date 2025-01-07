d64247965cfa18c222bc5388d56b3df64a57c5a0
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-07 19:49:24 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Footer from "./view/Footer";
 import Navbar from "./view/Navbar";
-import HomeView from "../Page/Home";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <HomeView/>
       <Footer/>
     </div>
   );
